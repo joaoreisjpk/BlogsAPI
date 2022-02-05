0e801c54dd5ace11ad10d21e4cf34b3f9671e97b
João Reis <joaoreisjpk@gmail.com>
2022-02-05 13:50:44 -0300

    doing the userRoute

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userRouter = require('./routes/user.routes');
 require('dotenv/config');
 
 const app = express();
@@ -6,7 +7,11 @@ const app = express();
 app.listen(process.env.PORT, () =>
   console.log(`ouvindo porta ${process.env.PORT}!`));
 
+app.use('/user', userRouter);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+module.exports = app;
