05e176d29cc22a6076f3489e6300f84cafe756a0
João Reis <joaoreisjpk@gmail.com>
2022-02-05 16:21:05 -0300

    validating the user services

--- services/UserServices.js
+++ services/UserServices.js
@@ -1,15 +1,29 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { Users } = require('../models');
+const Validate = require('../helpers/Validations');
 
 const secret = process.env.JWT_SECRET;
 const jwtConfig = { expiresIn: '1d', algorithm: 'HS256' };
 
 const createUser = async ({ displayName, email, password, image }) => {
-  await Users.create({ displayName, email, password, image });
+  try {
+    Validate.DisplayName(displayName);
+    Validate.Email(email);
+    Validate.Password(password);
 
-  const token = jwt.sign({ data: email }, secret, jwtConfig);
-  return { status: 201, response: { token } };
+    try {
+      await Users.create({ displayName, email, password, image });
+    } catch (err) {
+      return { status: 409, response: { message: 'User already registered' } };
+    }
+  
+    const token = jwt.sign({ data: email }, secret, jwtConfig);
+
+    return { status: 201, response: { token } };
+  } catch ({ message }) {
+    return { status: 400, response: { message } };
+  }
 };
 
 module.exports = { createUser };
