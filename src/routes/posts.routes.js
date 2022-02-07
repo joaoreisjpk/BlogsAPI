3d5b08030155138384147b56ec8eff0eec03bd7f
João Reis <joaoreisjpk@gmail.com>
2022-02-07 17:57:34 -0300

    adding the query router on posts routes

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -5,10 +5,23 @@ const userController = require('../controllers/UserController');
 const router = Router();
 
 router.post('/', userController.tokenValidation, postsController.createPost);
-// router.get('/:id', userController.tokenValidation, postsController.getPostsId);
+
 router.get('/:id', userController.tokenValidation, postsController.getPostId);
+
 router.get('/', userController.tokenValidation, postsController.getPosts);
+
 router.put('/:id', userController.tokenValidation, postsController.updatePost);
-router.delete('/:id', userController.tokenValidation, postsController.deletePost);
+
+router.delete(
+  '/:id',
+  userController.tokenValidation,
+  postsController.deletePost,
+);
+
+router.get(
+  '/search/q=:searchTerm',
+  userController.tokenValidation,
+  postsController.deletePost,
+);
 
 module.exports = router;
