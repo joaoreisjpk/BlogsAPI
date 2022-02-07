356100c283acef99adc06d847ffa9f7d5d0723e1
João Reis <joaoreisjpk@gmail.com>
2022-02-07 11:48:28 -0300

    creating the put method on post router

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -8,5 +8,6 @@ router.post('/', userController.tokenValidation, postsController.createPost);
 // router.get('/:id', userController.tokenValidation, postsController.getPostsId);
 router.get('/:id', userController.tokenValidation, postsController.getPostId);
 router.get('/', userController.tokenValidation, postsController.getPosts);
+router.put('/:id', userController.tokenValidation, postsController.getPosts);
 
 module.exports = router;
