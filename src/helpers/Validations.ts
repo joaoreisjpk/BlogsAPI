a5c2c50a9545fd62ad4d65f8c5e5cd8bc07a4654
João Reis <joaoreisjpk@gmail.com>
2022-02-09 09:44:15 -0300

    changing the helpers files name from JS to TS

--- src/helpers/Validations.ts
+++ src/helpers/Validations.ts
@@ -11,33 +11,33 @@ const noCategoryIDs = '"categoryIds" is required';
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-const { Categories } = require('../../models');
+import { Categories } from '../../models';
 
-const DisplayName = (name) => {
+const DisplayName = (name: string) => {
   if (!name || name.length < 8) throw Error(nameError);
 };
 
-const Email = (email) => {
+const Email = (email: string) => {
   if (!email && email !== '') throw Error(noEmail);
   if (!email) throw Error(blankEmail);
   if (!email.match(emailRegex)) throw Error(invalidEmail);
 };
 
-const Password = (password) => {
+const Password = (password:string) => {
   if (!password && password !== '') throw Error(noPassword);
   if (!password) throw Error(blankPassword);
   if (password.length !== 6) throw Error(invalidPassword);
 };
 
-const Title = (title) => {
+const Title = (title: string) => {
   if (!title) throw Error(noTitle);
 };
 
-const Content = (content) => {
+const Content = (content: string) => {
   if (!content) throw Error(noContent);
 };
 
-const CategoryIds = async (idsArray) => {
+const CategoryIds = async (idsArray: number[]) => {
   if (!idsArray) throw Error(noCategoryIDs);
 
   const { count } = await Categories.findAndCountAll({
@@ -55,4 +55,5 @@ const UpdateValidation = async ({ categoryIds, title, content }) => {
   if (!title) throw Error('"title" is required');
   if (!content) throw Error('"content" is required');
 };
-module.exports = { DisplayName, Email, Password, Content, Title, CategoryIds, UpdateValidation };
+
+export default { DisplayName, Email, Password, Content, Title, CategoryIds, UpdateValidation };
